2f03ecef6533c44abeeb8cdae5f6edc97f3b8cee
Julian Gabriel <148124142+muskmanelon@users.noreply.github.com>
2023-12-03 19:21:41 +0100

    sd

--- index.js
+++ index.js
@@ -1,64 +1,51 @@
-// app.js
 const express = require("express");
 const http = require("http");
-const socketIO = require("socket.io");
+const socketIo = require("socket.io");
+const Chess = require("chess");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server, { cors: { origin: "*" } });
+const io = socketIo(server, { cors: { origin: "*" } });
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public")); // Assuming your client-side code is in a folder named "public"
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
-const rooms = {};
+const games = {};
+let waitingPlayer = null;
 
 io.on("connection", (socket) => {
-  socket.on("createRoom", (roomName) => {
-    rooms[roomName] = { players: [socket.id], board: initialBoard() };
-    socket.join(roomName);
-    io.to(roomName).emit("updateGame", rooms[roomName].board);
-  });
-
-  socket.on("joinRoom", (roomName) => {
-    if (rooms[roomName] && rooms[roomName].players.length < 2) {
-      rooms[roomName].players.push(socket.id);
-      socket.join(roomName);
-      io.to(roomName).emit("updateGame", rooms[roomName].board);
+  socket.on("joinQueue", () => {
+    if (waitingPlayer) {
+      const gameKey = `${waitingPlayer.id}-${socket.id}`;
+      games[gameKey] = new Chess();
+      io.to(waitingPlayer.id).emit("startGame", gameKey, "white");
+      io.to(socket.id).emit("startGame", gameKey, "black");
+      waitingPlayer = null;
     } else {
-      socket.emit("errorMessage", "Room is full or does not exist");
+      waitingPlayer = { id: socket.id };
     }
   });
 
-  socket.on("move", ({ roomName, from, to }) => {
-    // Add chess logic to validate the move
-    // Update the board and broadcast the updated board to all players in the room
-    io.to(roomName).emit("updateGame", rooms[roomName].board);
+  socket.on("playerMove", (gameKey, move) => {
+    if (games[gameKey]) {
+      // Validate the move here if needed
+      // Update the game state
+      games[gameKey].move(move);
+      // Broadcast the move to the opponent
+      const opponentId = Object.keys(socket.rooms).find(
+        (id) => id !== socket.id
+      );
+      io.to(opponentId).emit("opponentMove", move);
+    }
   });
 
   socket.on("disconnect", () => {
-    for (const roomName in rooms) {
-      const index = rooms[roomName].players.indexOf(socket.id);
-      if (index !== -1) {
-        rooms[roomName].players.splice(index, 1);
-        io.to(roomName).emit("playerLeft", socket.id);
-        if (rooms[roomName].players.length === 0) {
-          delete rooms[roomName];
-        }
-      }
+    if (waitingPlayer && waitingPlayer.id === socket.id) {
+      waitingPlayer = null;
     }
   });
 });
 
-function initialBoard() {
-  // Implement your initial chess board state
-  // Return the initial board state
-}
-
-const port = process.env.PORT || 3000;
-
-server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
