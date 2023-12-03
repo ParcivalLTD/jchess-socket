df7553083a8e8904aef69feb6d83f7dab3a02c5b
Julian Gabriel <148124142+muskmanelon@users.noreply.github.com>
2023-12-03 18:57:14 +0100

    port

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
 });
 
-server.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
   console.log("listening on *:3000");
 });
