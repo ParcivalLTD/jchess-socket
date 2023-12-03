18f7c2082f02f80de644859679fb43e8fe9b441f
Magomed Alimkhanov <scrythepvp@gmail.com>
2023-12-03 23:08:25 +0100

    feat: fix code and make player move
    make some fixes to code and refactor and also that
    when a player moves, it will be emitted to room

--- index.js
+++ index.js
@@ -1,56 +1,38 @@
-// server.js
-const express = require("express");
 const http = require("http");
 const socketIO = require("socket.io");
 
-const app = express();
-const server = http.createServer(app);
+const server = http.createServer();
 const io = socketIO(server, { cors: { origin: "*" } });
-
-app.use(express.static("public"));
 
 const PORT = process.env.PORT || 3000;
 
-// Array to store players waiting for a match
 let waitingPlayers = [];
 
-io.on("connection", (socket) => {
+function userJoinRoom(io, socket) {
+  waitingPlayers.push(socket);
+  if (waitingPlayers.length < 2) return;
+  // remove player from watingPlayers list
+  const player1 = waitingPlayers.shift();
+  const player2 = waitingPlayers.shift();
+  const randRoomId = Math.ceil(Math.random() * 10000);
+  player1.join(randRoomId);
+  player2.join(randRoomId);
+  io.to(randRoomId).emit("startGame", { room: randRoomId });
+  console.log(`Game started in room ${randRoomId}`);
+}
+
+io.on("connection", async (socket) => {
   console.log("A user connected");
+  userJoinRoom(io, socket);
 
-  // Handle player joining the queue
-  socket.on("joinQueue", () => {
-    waitingPlayers.push(socket);
-
-    // Check if there are enough players to start a game
-    if (waitingPlayers.length >= 2) {
-      // Pair up the first two players and remove them from the waiting list
-      const player1 = waitingPlayers.shift();
-      const player2 = waitingPlayers.shift();
-
-      // Create a unique room ID
-      const roomId = `room-${Math.random().toString(36).substr(2, 9)}`;
-
-      // Join players to the room
-      player1.join(roomId);
-      player2.join(roomId);
-
-      // Notify players that the game is starting
-      io.to(roomId).emit("startGame", { room: roomId });
-
-      console.log(`Game started in room ${roomId}`);
-    }
-  });
-
-  // Handle player move
-  socket.on("playerMove", (move) => {
+  socket.on("playerMove", ({ move, room }) => {
     // Broadcast the move to the opponent in the same room
-    socket.to(move.room).emit("opponentMove", move);
+    socket.to(room).emit("opponentMove", move);
+    console.log("move emit");
   });
 
-  // Handle disconnect
   socket.on("disconnect", () => {
     console.log("A user disconnected");
-
     // Remove the player from the waiting list if they were in the queue
     waitingPlayers = waitingPlayers.filter((player) => player !== socket);
   });
