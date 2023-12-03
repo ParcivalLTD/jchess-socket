2260152d5c23405fcb556d84e83aa09c5570bced
Julian Gabriel <148124142+muskmanelon@users.noreply.github.com>
2023-12-03 19:41:36 +0100

    sdad

--- index.js
+++ index.js
@@ -8,63 +8,41 @@ const io = socketIO(server, { cors: { origin: "*" } });
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("public")); // Assuming your client-side files are in a "public" directory
+app.use(express.static(__dirname + "/public"));
 
-const games = {};
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+const rooms = {};
+let waitingPlayer = null;
 
 io.on("connection", (socket) => {
-  // Handle player joining the queue
-  socket.on("joinQueue", () => {
-    let gameId;
-
-    // Check if there's a game waiting for an opponent
-    const waitingGame = Object.values(games).find((game) => !game.player2);
-
-    if (waitingGame) {
-      gameId = waitingGame.id;
-      waitingGame.player2 = socket.id;
-      io.to(socket.id).emit("gameFound", { gameId, color: "black" });
-      io.to(waitingGame.player1).emit("gameFound", { gameId, color: "white" });
+  socket.on("queue", () => {
+    if (waitingPlayer) {
+      const room = socket.id + "-" + waitingPlayer.id;
+      socket.join(room);
+      waitingPlayer.join(room);
+      io.to(room).emit("startGame");
+      waitingPlayer = null;
     } else {
-      // Create a new game
-      gameId = Date.now().toString();
-      games[gameId] = { id: gameId, player1: socket.id, player2: null };
-      io.to(socket.id).emit("waitingForOpponent", gameId);
+      waitingPlayer = socket;
     }
-
-    // Join the room for the game
-    socket.join(gameId);
   });
 
-  // Handle player moves
   socket.on("playerMove", (move) => {
-    const game = Object.values(games).find(
-      (g) => g.player1 === socket.id || g.player2 === socket.id
-    );
-
-    if (game) {
-      const opponentSocketId =
-        game.player1 === socket.id ? game.player2 : game.player1;
-      io.to(opponentSocketId).emit("opponentMove", move);
-    }
+    socket.to(socket.room).emit("opponentMove", move);
   });
 
-  // Handle disconnect
   socket.on("disconnect", () => {
-    // Find the game the player is in and inform the opponent
-    const game = Object.values(games).find(
-      (g) => g.player1 === socket.id || g.player2 === socket.id
-    );
-
-    if (game) {
-      const opponentSocketId =
-        game.player1 === socket.id ? game.player2 : game.player1;
-      io.to(opponentSocketId).emit("opponentLeft");
-      delete games[game.id];
+    if (waitingPlayer && waitingPlayer.id === socket.id) {
+      waitingPlayer = null;
+    } else {
+      io.to(socket.room).emit("opponentLeft");
     }
   });
 });
 
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
